5ed49c9c6560fb6ca1c2ef6ae3571acd909db73d
Mark Veenvliet <M.Veenvliet@cimsolutions.nl>
2017-09-14 13:48:27 +0200

    changes hello world

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,8 +1,20 @@
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-public class HelloWorld 
-{
-public static void main(String args[]) 
-	{
+public class HelloWorld {
+	public static void main(String args[]) throws IOException, InterruptedException {
+		char ch;
+
 		System.out.println("Hello world!");
+		while (true) {
+
+			ch = (char) System.in.read();
+			// TimeUnit.SECONDS.sleep(1);
+			if (ch > 'A' && ch < 'z')
+				System.out.println("You pressed: " + ch);
+			else if (ch == '\n')
+				System.out.println("Return pressed");
+		}
+
 	}
 }
