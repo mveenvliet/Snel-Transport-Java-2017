53d992f9f671dd25609f3dbb89323826b619ce3b
jobo03 <j.bouwman@cimsolutions.nl>
2017-10-17 21:19:01 +0200

    Controller fix

--- Software/Snel Transport Web-application/SnelTransport/src/main/java/CIMSOLUTIONS/SnelTransport/PlaceOrder/PlaceOrderController.java
+++ Software/Snel Transport Web-application/SnelTransport/src/main/java/CIMSOLUTIONS/SnelTransport/PlaceOrder/PlaceOrderController.java
@@ -1,6 +1,7 @@
 package CIMSOLUTIONS.SnelTransport.PlaceOrder;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,27 +10,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import static java.util.Arrays.asList;
 
 @RestController
 public class PlaceOrderController {
+
 	int customerNumber;
 	String companyName;
 	String firstname;
 	String lastname;
 	
-	@RequestMapping(value="/postCustomerController", method=RequestMethod.POST)
-	@ResponseBody public String postCustomer(@RequestBody Customer customer){
+	@RequestMapping(value="/postCustomerController", method=RequestMethod.GET)
+	@ResponseBody
+	public List<Customer> postCustomer(){
 		
-		searchCustomerService searchCustomer = new searchCustomerService();
-//		List<Customer> retList= new ArrayList<Customer>();
-		searchCustomer.lookUpCustomer(customer);
-//		List<String> retList= new ArrayList<>();
-//		retList.add("test1");
-//		retList.add("test2");
-//		retList = searchCustomer.getResultSet();
-//		return retList;
-		return searchCustomer.getResultSet().get(0).getInfo();
+//		searchCustomerService searchCustomer = new searchCustomerService();
+//		searchCustomer.lookUpCustomer(customer);
+//		return searchCustomer.getResultSet().get(0).getInfo();
+
+		Customer klant1 = new Customer(1, "BedrijfA", "A", "Appel");
+		Customer klant2 = new Customer(2, "BedrijfB", "B", "Banaan");
+		return asList(klant1, klant2);
 	}
 
 }
